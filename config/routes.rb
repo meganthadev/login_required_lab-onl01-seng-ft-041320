3232fc84fd19ac6524027cba27535911a9d68b2f
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 01:54:46 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  get 'secrets/show'
   resources :sessions, only: [:create]
   get 'destroy' => 'sessions#destroy'
   get 'login' => 'sessions#new', as: 'new_session'
