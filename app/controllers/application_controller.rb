f4fc37d8a88bb86c9314d4a00cc3149e8201b874
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 01:51:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   
   def current_user 
     session[:name]
-    if session[:name]
->>>>>>> 41896c5c8bebac2cd4ba163b3ef626f1dd2974ce
+    41896c5c8bebac2cd4ba163b3ef626f1dd2974ceif session[:name]
+>>>>>>> 
   end   
   
 end
