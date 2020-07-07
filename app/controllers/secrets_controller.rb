5f69267fba8942af260f451694ac027c6472f2fb
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 01:26:47 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,13 @@
+class SecretsController < ApplicationController 
+  before_action :authenticate
+
+  def show
+  end
+
+  private
+
+  def authenticate
+    redirect_to new_session_path if current_user.nil? or current_user == ""
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+  get 'destroy' => 'sessions#destroy'
+  get 'login' => 'sessions#new', as: 'new_session'
+  root 'application#index'
+  
 end
